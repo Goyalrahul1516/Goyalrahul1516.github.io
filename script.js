b3be9727bef7d65cee6bf623cafaa5413ab02b02
Rahul Goyal <goyalrahul1516@gmail.com>
2025-03-31 13:12:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Dynamically add projects to the portfolio
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = [
+        { title:'Low Power FPGA Synthesizer' ,desc:'Energy-efficient circuit on FPGA'} 
+    ]
+    
+})
